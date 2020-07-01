00224f5f70f27078a320e62898fba89fc56c671b
Sanjeev Kumar <sanjeev.kumar@tothenew.com>
2020-07-01 23:31:32 +0530

    Dispatching Actions

--- redux-basics.js
+++ redux-basics.js
@@ -8,6 +8,20 @@ const intialState = {
 // Reducer
 // here initialState assign as a default value
 const rootReducer = (state = intialState, action) => { 
+    if(action.type === 'INC_COUNTER'){
+        return{
+            ...state,
+            counter: state.counter + 1
+        };
+    }
+
+    if(action.type === 'ADD_COUNTER'){
+        return{
+            ...state,
+            counter: state.counter + action.value
+        };
+    }
+
     return state;
 };
 
@@ -16,3 +30,8 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Dispatching Action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
+
+// Subscription
